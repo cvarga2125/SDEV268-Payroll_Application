06da044668097b7cb194acad087042efb30994e2
cvarga2125 <cvarga2@ivytech.edu>
2024-12-15 17:31:13 -0500

    Main commit

---
+++ src/main/App.java
@@ -0,0 +1,10 @@
+package main;
+
+import main.Login.Login;
+
+public class App {
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.displayLogin();
+    }
+}
